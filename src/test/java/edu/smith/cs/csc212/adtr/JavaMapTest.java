caf5378f7f134812441c327f3d43dd3a2e7190a9
MackieZhou <mzhou@smith.edu>
2020-02-28 17:00:01 -0500

    complete set and map test

--- src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
@@ -6,10 +6,11 @@ import org.junit.Test;
 import edu.smith.cs.csc212.adtr.real.JavaMap;
 
 public class JavaMapTest {
-	
+
 	// You might want this; if you're using Map<String, Integer> anywhere...
 	// JUnit has an assertEquals(Object, Object) and an assertEquals(int, int).
-	// When you give it assertEquals(Integer, int) it doesn't know which to use (but both would be OK!)
+	// When you give it assertEquals(Integer, int) it doesn't know which to use (but
+	// both would be OK!)
 	// This method gets around that by forcing the (int, int) version.
 	void assertIntEq(int x, int y) {
 		assertEquals(x, y);
@@ -19,6 +20,51 @@ public class JavaMapTest {
 	public void testEmpty() {
 		MapADT<String, String> empty = new JavaMap<>();
 		assertEquals(empty.size(), 0);
-		//Assert.assert
+		// Assert.assert
+	}
+
+	@Test
+	public void testSize() {
+		MapADT<String, String> things = new JavaMap<>();
+		// size should give the number of keys
+		things.put("dog", "black dog");
+		assertFalse(things.size() == 2);
+		assertTrue(things.size() == 1);
+	}
+
+	@Test
+	public void testPut() {
+		MapADT<String, String> things = new JavaMap<>();
+		things.put("dog", "black dog");
+		things.put("fish", "salmon");
+		assertTrue(things.size() == 2);	
+	}
+
+	@Test
+	public void testGet() {
+		MapADT<String, String> things = new JavaMap<>();
+		things.put("dog", "black dog");
+		things.put("fish", "salmon");
+		assertEquals(things.get("fish"), "salmon");
+		assertEquals(things.get("dog"), "black dog");
+
+		// the new value of the same key overwrites the first value
+		things.put("dog", "white dog");
+		assertFalse(things.get("dog") == "black dog");
+	}
+
+	@Test
+	public void testRemove() {
+		MapADT<String, String> things = new JavaMap<>();
+		things.put("dog", "black dog");
+		things.put("fish", "salmon");
+
+		// remove using a key
+		things.remove("fish");
+		assertNull(things.get("fish"));
+
+		// if you want to remove using a value, it shouldn't work
+		things.remove("black dog");
+		assertEquals(things.get("dog"), "black dog");
 	}
 }
--- src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
@@ -1,17 +1,67 @@
 package edu.smith.cs.csc212.adtr;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import edu.smith.cs.csc212.adtr.real.JavaSet;
 
 public class JavaSetTest {
-	
+
 	@Test
 	public void testEmpty() {
 		SetADT<String> empty = new JavaSet<>();
 		assertEquals(empty.size(), 0);
 	}
-	
+
+	@Test
+	public void testInsertOneThing() {
+		// it's impossible to test size fully without also calling insert...
+		SetADT<String> one = new JavaSet<>();
+		one.insert("A");
+		assertEquals(one.size(), 1);
+	}
+
+	@Test
+	public void testInsertRepeated() {
+		SetADT<String> pets = new JavaSet<>();
+		assertEquals(pets.size(), 0);
+		pets.insert("dog");
+		assertEquals(pets.size(), 1);
+		pets.insert("goldfish");
+		assertEquals(pets.size(), 2);
+		pets.insert("dog");
+		assertEquals(pets.size(), 2);
+	}
+
+	@Test
+	public void testContains() {
+		SetADT<String> things = new JavaSet<>();
+		things.insert("A");
+		things.insert("B");
+		things.insert("C");
+		things.insert("D");
+		things.insert("E");
+		assertFalse(things.contains("2"));
+		assertTrue(things.contains("E"));
+	}
+
+	@Test
+	public void testRemove() {
+		SetADT<String> things = new JavaSet<>();
+		things.insert("bamboo");
+		things.insert("sunlight");
+		things.remove("bamboo");
+		assertFalse(things.contains("banboo"));
+		assertTrue(things.contains("sunlight"));
+		assertFalse(things.contains("moon"));
+
+		// or you can do this
+		SetADT<String> expected = new JavaSet<>();
+		expected.insert("sunlight");
+		assertEquals(things, expected);
+	}
+
 }
