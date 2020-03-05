54f3596bd596f5657390b428c4b2a5da668031f1
MackieZhou <mzhou@smith.edu>
2020-03-04 23:44:29 -0500

    challenges

--- src/main/java/edu/smith/cs/csc212/adtr/Challenges.java
+++ src/main/java/edu/smith/cs/csc212/adtr/Challenges.java
@@ -4,23 +4,68 @@ import edu.smith.cs.csc212.adtr.real.JavaMap;
 import edu.smith.cs.csc212.adtr.real.JavaSet;
 
 // Wow, this class really needs some comments...
+/**
+ * This class gives you union, intersection, or word count of sets of strings
+ * 
+ * @author MackieZhou
+ *
+ */
 public class Challenges {
 
+	/**
+	 * output the union of two sets of strings
+	 * 
+	 * @param left  - the first set of strings
+	 * @param right - the second set of strings
+	 * @return the union of left and right
+	 */
 	public static SetADT<String> union(SetADT<String> left, SetADT<String> right) {
 		SetADT<String> output = new JavaSet<>();
-		
+		// add everything in left into union
+		for (String i : left) {
+			output.insert(i);
+		}
+		// add everything in right into union
+		for (String i : right) {
+			output.insert(i);
+		}
 		return output;
 	}
-	
+
+	/**
+	 * output the intersection of two sets of strings
+	 * 
+	 * @param left  - the first set of strings
+	 * @param right - the second set of strings
+	 * @return the intersection of left and right
+	 */
 	public static SetADT<String> intersection(SetADT<String> left, SetADT<String> right) {
 		SetADT<String> output = new JavaSet<>();
-		
+		for (String i : left) {
+			if (right.contains(i)) {
+				output.insert(i);
+			}
+		}
 		return output;
 	}
-	
+
+	/**
+	 * count the number of existence of each word in the input list of strings
+	 * 
+	 * @param words - a list of words that you want to count
+	 * @return map - key is the word, value is the count
+	 */
 	public static MapADT<String, Integer> wordCount(ListADT<String> words) {
 		MapADT<String, Integer> output = new JavaMap<>();
-		
+
+		for (String i : words) {
+			if (output.get(i) == null) {
+				output.put(i, 1);
+			} else {
+				output.put(i, output.get(i) + 1);
+			}
+		}
+
 		return output;
 	}
 }
--- src/test/java/edu/smith/cs/csc212/adtr/ChallengesTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/ChallengesTest.java
@@ -13,38 +13,153 @@ public class ChallengesTest {
 	public void testUnionSimple() {
 		SetADT<String> left = new JavaSet<>();
 		SetADT<String> right = new JavaSet<>();
-		
+
 		left.insert("A");
 		left.insert("B");
 		right.insert("B");
 		right.insert("C");
-		
+
 		SetADT<String> expected = new JavaSet<>();
 		expected.insert("A");
 		expected.insert("B");
 		expected.insert("C");
+
 		Assert.assertEquals(expected, Challenges.union(left, right));
 	}
-	
+
+	@Test
+	public void testUnionNoRepeat() {
+		SetADT<String> left = new JavaSet<>();
+		SetADT<String> right = new JavaSet<>();
+
+		left.insert("A");
+		left.insert("B");
+		right.insert("C");
+		right.insert("D");
+
+		SetADT<String> expected = new JavaSet<>();
+		expected.insert("A");
+		expected.insert("B");
+		expected.insert("C");
+		expected.insert("D");
+
+		Assert.assertEquals(expected, Challenges.union(left, right));
+	}
+
+	@Test
+	public void testUnionSameSets() {
+		SetADT<String> left = new JavaSet<>();
+		left.insert("A");
+		left.insert("B");
+
+		SetADT<String> expected = new JavaSet<>();
+		expected.insert("A");
+		expected.insert("B");
+
+		Assert.assertEquals(expected, Challenges.union(left, left));
+	}
+
+	@Test
+	public void testUnionOneEmpty() {
+		SetADT<String> left = new JavaSet<>();
+		SetADT<String> right = new JavaSet<>();
+		left.insert("A");
+		left.insert("B");
+
+		SetADT<String> expected = new JavaSet<>();
+		expected.insert("A");
+		expected.insert("B");
+
+		Assert.assertEquals(expected, Challenges.union(left, right));
+	}
+
+	@Test
+	public void testUnionBothEmpty() {
+		SetADT<String> left = new JavaSet<>();
+		SetADT<String> right = new JavaSet<>();
+		SetADT<String> expected = new JavaSet<>();
+
+		Assert.assertEquals(expected, Challenges.union(left, right));
+	}
+
 	@Test
 	public void testIntersectionSimple() {
 		SetADT<String> left = new JavaSet<>();
 		SetADT<String> right = new JavaSet<>();
-		
+
 		left.insert("A");
 		left.insert("B");
 		right.insert("B");
 		right.insert("C");
-		
+
 		SetADT<String> expected = new JavaSet<>();
 		expected.insert("B");
+
 		Assert.assertEquals(expected, Challenges.intersection(left, right));
 	}
-	
+
+	@Test
+	public void testIntersectionNoRepeat() {
+		SetADT<String> left = new JavaSet<>();
+		SetADT<String> right = new JavaSet<>();
+
+		left.insert("A");
+		left.insert("B");
+		right.insert("C");
+
+		SetADT<String> expected = new JavaSet<>();
+
+		Assert.assertEquals(expected, Challenges.intersection(left, right));
+	}
+
+	@Test
+	public void testIntersectionSubset() {
+		SetADT<String> left = new JavaSet<>();
+		SetADT<String> right = new JavaSet<>();
+
+		left.insert("A");
+		left.insert("B");
+		left.insert("C");
+		left.insert("D");
+		right.insert("B");
+		right.insert("C");
+
+		SetADT<String> expected = new JavaSet<>();
+		expected.insert("B");
+		expected.insert("C");
+
+		Assert.assertEquals(expected, Challenges.intersection(left, right));
+	}
+
+	@Test
+	public void testIntersectionOneEmpty() {
+		SetADT<String> left = new JavaSet<>();
+		SetADT<String> right = new JavaSet<>();
+
+		left.insert("A");
+		left.insert("B");
+		left.insert("C");
+		left.insert("D");
+
+		SetADT<String> expected = new JavaSet<>();
+
+		Assert.assertEquals(expected, Challenges.intersection(left, right));
+	}
+
+	@Test
+	public void testIntersectionBothEmpty() {
+		SetADT<String> left = new JavaSet<>();
+		SetADT<String> right = new JavaSet<>();
+
+		SetADT<String> expected = new JavaSet<>();
+
+		Assert.assertEquals(expected, Challenges.intersection(left, right));
+	}
+
 	@Test
 	public void testWordCountSimple() {
 		ListADT<String> example = new JavaList<>();
-		for (String word : new String[] {"a", "b", "a", "b", "c", "d"}) {
+		for (String word : new String[] { "a", "b", "a", "b", "c", "d" }) {
 			example.addBack(word);
 		}
 		MapADT<String, Integer> expected = new JavaMap<>();
@@ -52,7 +167,26 @@ public class ChallengesTest {
 		expected.put("b", 2);
 		expected.put("c", 1);
 		expected.put("d", 1);
-		
+
+		Assert.assertEquals(expected, Challenges.wordCount(example));
+	}
+
+	@Test
+	public void testWordCountEmpty() {
+		ListADT<String> example = new JavaList<>();
+		MapADT<String, Integer> expected = new JavaMap<>();
+
+		Assert.assertEquals(expected, Challenges.wordCount(example));
+	}
+
+	@Test
+	public void testWordCountOneEntry() {
+		ListADT<String> example = new JavaList<>();
+		example.addBack("a");
+
+		MapADT<String, Integer> expected = new JavaMap<>();
+		expected.put("a", 1);
+
 		Assert.assertEquals(expected, Challenges.wordCount(example));
 	}
 
